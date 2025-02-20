a7d3699eb125b91585a2fb9dad8c9546d8dd918b
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2025-02-20 17:30:14 +0200

    update RepoInput

--- my-github-issues-board/src/components/RepoInput.tsx
+++ my-github-issues-board/src/components/RepoInput.tsx
@@ -1,57 +1,60 @@
-import React, { useState } from 'react';
-import { Input, Button, Space, Typography } from 'antd';
+import React, { useState } from "react";
+import { Input, Button, Space, Typography } from "antd";
 
 const { Link } = Typography;
 
 interface Repo {
-  owner: string;
-  name: string;
+    owner: string;
+    name: string;
 }
 
 interface RepoInputProps {
-  onLoadRepo: (url: string) => void;
-  repo: Repo | null;
+    onLoadRepo: (url: string) => void;
+    repo: Repo | null;
 }
 
 const RepoInput: React.FC<RepoInputProps> = ({ onLoadRepo, repo }) => {
-  const [url, setUrl] = useState('');
+    const [url, setUrl] = useState("");
 
-  const handleLoad = () => {
-    if (url) {
-      onLoadRepo(url);
-    }
-  };
+    const handleLoad = () => {
+        if (url) {
+            onLoadRepo(url);
+        }
+    };
 
-  return (
-    <div className="repo-input-container">
-      <Space>
-        <Input
-          placeholder="Enter repo URL (e.g. https://github.com/facebook/react)"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          style={{ width: 400 }}
-        />
-        <Button type="primary" onClick={handleLoad}>
-          Load issues
-        </Button>
-      </Space>
-      {repo && (
-        <div className="repo-links">
-          <Space direction="vertical">
-            <Link href={`https://github.com/${repo.owner}`} target="_blank">
-              Профіль власника
-            </Link>
-            <Link
-              href={`https://github.com/${repo.owner}/${repo.name}`}
-              target="_blank"
-            >
-              Сторінка репозиторію
-            </Link>
-          </Space>
+    return (
+        <div className="repo-input-container">
+            <Space>
+                <Input
+                    placeholder="Enter repo URL (e.g. https://github.com/facebook/react)"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                    style={{ width: 400 }}
+                />
+                <Button className="load-issues-button" onClick={handleLoad}>
+                    Load issues
+                </Button>
+            </Space>
+            {repo && (
+                <div className="repo-links">
+                    <Space direction="vertical">
+                        <Link
+                            href={`https://github.com/${repo.owner}`}
+                            target="_blank"
+                        >
+                            Профіль власника
+                        </Link>
+                        <Link
+                            href={`https://github.com/${repo.owner}/${repo.name}`}
+                            target="_blank"
+                        >
+                            Сторінка репозиторію
+                        </Link>
+                    </Space>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default RepoInput;
